f52e9d368726bd419735e15b5f50fbe62959e416
Olegqqw <92752734+NikoFan@users.noreply.github.com>
2024-03-20 11:22:13 +0300

    Начал работу с окном пользователя

--- VR-registration/ConnectDataBase.cs
+++ VR-registration/ConnectDataBase.cs
@@ -43,7 +43,7 @@ namespace VR_registration
         public ProgramCashReader programCashReader = new ProgramCashReader();
         public ControlInputData controlInputData = new ControlInputData();
         
-        public string ConnectionString = "data source=7.tcp.eu.ngrok.io, 15228;" +
+        public string ConnectionString = "data source=5.tcp.eu.ngrok.io, 18580;" +
             "Database=VR_club;" +
             "User Id=sa;" +
             "Password=7784865Oleg#;" +
